ce5260d32bac390a7858f0c88c716633542da976
Djurryen Nieuwendijk <djurryen.nieuwendijk@student.hu.nl>
2019-03-16 13:02:46 +0100

    tabel begin

--- src/ontvangen-ingredienten/ontvangen-ingredienten.js
+++ src/ontvangen-ingredienten/ontvangen-ingredienten.js
@@ -1,4 +1,6 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@polymer/paper-input/paper-input.js';
+
 
 /**
  * @customElement
@@ -12,31 +14,89 @@ class OntvangenIngredienten extends PolymerElement {
           display: block;
         }
 
-        #Tabel{
-          border: 1px black solid;
+        #tabel{
+          border
+        }
+
+        #trH{
+          background-color: #9E9E9E;
+        }
+
+        #trO{
+          background-color: #E6E6E6;
+        }
+        #trE{
+          background-color: #FFFFFF;
+        }
+
+        #td0{
+          width: 10em;
+        }
+
+        #td1{
+          width: 25em;
+        }
+
+        #td2{
+          width: 65em;
         }
       </style>
 
-      <table id="Tabel">
-        <tr>
-          <th>Test</th>
-          <th>Test</th>
-          <th>Test</th>
+      <table id="tabel">
+        <tr id="trH">
+          <th id="td0"></th>
+          <th id="td1">Ingrediënt</th>
+          <th id="td2">Hoeveelheid</th>
         </tr>
-        <tr>
-          <td>Jill</td>
-          <td>Smith</td>
-          <td>50</td>
+        <tr id="trO">
+          <td id="td0"></td>
+          <td id="td1">[[ingredient.0]]</td>
+          <td id="td2">35<paper-input value="{{aantal-Zalm}}"></paper-input><button id="but1">Toevoegen</td>
         </tr>
-        <tr>
-          <td>Eve</td>
-          <td>Jackson</td>
-          <td>94</td>
+        <tr id="trE">
+          <td id="td0"></td>
+          <td id="td1">[[ingredient.1]]</td>
+          <td id="td2">20<paper-input value="{{aantal-Tonijn}}"></paper-input><button id="but1">Toevoegen</td>
         </tr>
-        <tr>
-          <td>John</td>
-          <td>Doe</td>
-          <td>80</td>
+        <tr id="trO">
+          <td id="td0"></td>
+          <td id="td1">[[ingredient.2]]</td>
+          <td id="td2">15<paper-input value="{{aantal-Kip}}"></paper-input><button id="but1">Toevoegen</td>
+        </tr>
+        <tr id="trE">
+          <td id="td0"></td>
+          <td id="td1">[[ingredient.3]]</td>
+          <td id="td2">10<paper-input value="{{aantal-Komkommer}}"></paper-input><button id="but1">Toevoegen</td>
+        </tr>
+        <tr id="trO">
+          <td id="td0"></td>
+          <td id="td1">[[ingredient.4]]</td>
+          <td id="td2">35<paper-input value="{{aantal-Krab}}"></paper-input><button id="but1">Toevoegen</td>
+        </tr>
+        <tr id="trE">
+          <td id="td0"></td>
+          <td id="td1">[[ingredient.5]]</td>
+          <td id="td2">24<paper-input value="{{aantal-Wortel}}"></paper-input><button id="but1">Toevoegen</td>
+        </tr>
+        <tr id="trO">
+          <td id="td0"></td>
+          <td id="td1">[[ingredient.6]]</td>
+          <td id="td2">23<paper-input value="{{aantal-Ui}}"></paper-input><button id="but1">Toevoegen</td>
+        </tr>
+        <tr id="trE">
+          <td id="td0"></td>
+          <td id="td1">[[ingredient.7]]</td>
+          <td id="td2">12<paper-input value="{{aantal-Sla}}"></paper-input><button id="but1">Toevoegen</td>
+        </tr>
+        <tr id="trO">
+          <td id="td0"></td>
+          <td id="td1">[[ingredient.8]]</td>
+          <td id="td2">45<paper-input value="{{aantal-Zeewierbladen}}"></paper-input><button id="but1">Toevoegen</td>
+        </tr>
+        <tr id="trE">
+          <td id="td0"></td>
+          <td id="td1">[[ingredient.9]]</td>
+          <td id="td2">21<paper-input value="{{aantal-Tomaat}}"></paper-input><button id="but1">Toevoegen</td>
         </tr>
       </table>
 
@@ -44,9 +104,20 @@ class OntvangenIngredienten extends PolymerElement {
   }
   static get properties() {
     return {
-      prop1: {
-        type: String,
-        value: 'ontvangen-ingredienten'
+      ingredient: {
+        type: Array,
+        value: [
+          "Zalm",
+          "Tonijn",
+          "Kip",
+          "Krab",
+          "Komkommer",
+          "Wortel",
+          "Ui",
+          "Sla",
+          "Zeewierbladen",
+          "Tomaat"
+        ]
       }
     };
   }
